065ce8ec45363924341592ad26719ae8618a741c
Imam Arief <imam.crackerz999@gmail.com>
2023-10-05 09:14:14 +0700

    Membuat Model & Migration Tabel Cart

---
+++ app/Models/Cart.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'users_id',
+        'products_id',
+        'created_at',
+        'updated_at'
+    ];
+}
