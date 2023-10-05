1ebc7b94cd607104253d55b5a9b093d712d9f5e9
Imam Arief <imam.crackerz999@gmail.com>
2023-10-05 11:12:13 +0700

    Membuat halaman frontend dengan FrontendController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.frontend');
-});
+Route::get('/', [FrontendController::class, 'index'])->name('index');
 
 Route::middleware([
     'auth:sanctum',
