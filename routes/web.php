159abb885723ebc31c7d2a084062066c3abf0b01
Imam Arief <imam.crackerz999@gmail.com>
2023-10-05 23:41:23 +0700

    Membuat Halaman Frontend Checkout Success

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\FrontendController;
 Route::get('/', [FrontendController::class, 'index'])->name('index');
 Route::get('/details/{slug}', [FrontendController::class, 'details'])->name('details');
 Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
+Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
 
 Route::middleware([
     'auth:sanctum',
