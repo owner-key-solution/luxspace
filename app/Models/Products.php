d96684ee0d5db7af6d89bc89cbfa32b8e0a132d2
Imam Arief <imam.crackerz999@gmail.com>
2023-10-05 09:07:02 +0700

    Membuat Migration & Model Product

---
+++ app/Models/Products.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Products extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'slug',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
