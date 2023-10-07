a560c1e3161be62a3661d3ae22853fe6ddccdf97
Imam Arief <imam.crackerz999@gmail.com>
2023-10-07 07:51:21 +0700

    Membuat Form Request untuk Product

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,12 @@ Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('das
     Route::get('/', [DashboardController::class, 'index'])->name('index');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
